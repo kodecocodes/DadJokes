e6f3e57956fbc2e8ffe9a382fa81876f936c163b
nightlite <ldgryl@gmail.com>
2018-03-23 14:09:28 +0100

    Added new joke

--- DadJokes/JokeStore.swift
+++ DadJokes/JokeStore.swift
@@ -33,6 +33,7 @@ struct JokeStore {
     Joke(description: "Why don't programmers like being outside?", punchline: "Because there's too many bugs"),
     Joke(description: "Why don't bachelors like Git?", punchline: "Because they are afraid to commit."),
     Joke(description: "What does the father play with his son?", punchline: "Try Catch"),
+    Joke(description: "How many programmers does it take to change a lightbulb?", punchline: "None, that's a hardware problem!"),
   ]
 }
 
